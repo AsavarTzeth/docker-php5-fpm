760c6a62122871247906b796aa40bac50045b8a6
Alex Andrascu <alex.a@adappt.co.uk>
2015-01-15 16:13:44 +0000

    Adding environment variables support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,4 +20,23 @@ set_config 'memory_limit' "$PHP5_FPM_MEMORY_LIMIT"
 set_config 'log_level' "$PHP5_FPM_LOG_LEVEL"
 set_config 'listen' "$PHP5_FPM_LISTEN"
 
+# Function to update the fpm configuration to make the service environment variables available
+function setEnvironmentVariable() {
+
+    if [ -z "$2" ]; then
+            echo "Environment variable '$1' not set."
+            return
+    fi
+
+    # Add variable
+    echo "env[$1] = $2" >> /etc/php5/fpm/pool.d/www.conf
+}
+
+# Grep for variables that look like docker set them (_PORT_)
+for _curVar in `env | grep _PORT_ | awk -F = '{print $1}'`;do
+    # awk has split them by the equals sign
+    # Pass the name and value to our function
+    setEnvironmentVariable ${_curVar} ${!_curVar}
+done
+
 exec "$@"
